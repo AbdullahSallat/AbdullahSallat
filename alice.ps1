1e7a3b7020e476ee5ee0fa6673fe26d2dc325fa7
Abdullah Sallat <abdullah.sallat.student@ksgyf.se>
2025-04-29 09:08:14 +0200

    variablier

---
+++ alice.ps1
@@ -0,0 +1,9 @@
+#skapa två veariabler och lägg ihop dem för att sedan skriva ut.
+
+#variabler för namn
+$namn = "Alice"
+#variablier för ålder
+$ålder = 25
+
+#print varabilar 
+Write-Host "Hej $namn, du är $ålder är gammal"
